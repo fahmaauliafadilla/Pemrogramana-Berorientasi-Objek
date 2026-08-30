5839fee200f3ce33c374ea5883f905d401eed667
fahmaauliafadilla <fahma.aukia.fadilla@students.untidar.ac.id>
2026-08-30 10:44:10 +0700

    Add files via upload

---
+++ Fahma_project1.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.fahma_project1;
+
+/**
+ *
+ * @author m s i
+ */
+public class Fahma_project1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+        String Nama = "Fahma Aulia Fadilla";
+        String NPM = "2505060015";
+        String Alamat = "Bayat, Klaten";
+        String No_Hp = "085677996666";
+        
+        
+        System.out.printf(" Nama : %s%n ", Nama);
+        System.out.printf(" NPM : %s%n", NPM);
+        System.out.printf(" Alamat : %s%n", Alamat);
+        System.out.printf("No_Hp : %s%n", No_Hp);
+                
+    }
+}
