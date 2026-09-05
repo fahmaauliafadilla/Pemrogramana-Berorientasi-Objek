320d92393c991e9ab5cfe5ef92186a97e3cdc9fc
fahmaauliafadilla <fahma.aukia.fadilla@students.untidar.ac.id>
2026-09-05 09:48:24 +0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package tugas3;
+package praktikum3;
 
 /**
  *
@@ -10,22 +10,11 @@ package tugas3;
  */
 public class Main {
     public static void main(String[] args) {
-        Mobil mobil1 = new Mobil("Toyota", "Avanza", 2020, "Hitam");
-        Mobil mobil2 = new Mobil("Honda", "Civic", 2022, "Putih");
-
-        mobil1.startEngine();
-        mobil2.startEngine();
-
-        System.out.println("\n=== Kondisi Awal ===");
-        mobil1.displayInfo();
-        mobil2.displayInfo();
-
-        System.out.println("\n=== Mengubah Warna Mobil ===");
-        mobil1.setWarna("Merah");
-        mobil2.setWarna("Abu-abu");
-
-        System.out.println("\n=== Kondisi Setelah Perubahan Warna ===");
-        mobil1.displayInfo();
-        mobil2.displayInfo();
-    }  
+        Hewan kucing = new Hewan("Xavier", 3);
+        kucing.suara();
+        kucing.info();
+        Hewan anjing = new Hewan("Puppy", 2);
+        anjing.berlari();
+        anjing.info();
+    }   
 }
